863d3795bbccc9908b00a8d71f2dbf96fcb17958
Tiago-0liveira <tfgoliveira@gmail.com>
2021-06-23 14:50:05 +0100

    improved code and support for the new LIGHTNING bow

--- src/main/java/me/tiago0liveira/TiagoUtils/events/onArrowCollides.java
+++ src/main/java/me/tiago0liveira/TiagoUtils/events/onArrowCollides.java
@@ -9,6 +9,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 
 public class onArrowCollides implements Listener {
@@ -19,15 +21,22 @@ public class onArrowCollides implements Listener {
             Player player = (Player) e.getEntity().getShooter();
             if (TiagoUtils.options.getConfigurationSection("events").getBoolean("onArrowCollides")) {
                 ItemStack bow = player.getInventory().getItemInMainHand();
-                String bowType = bow.getItemMeta().getPersistentDataContainer().get(
-                        new NamespacedKey(TiagoUtils.getPlugin(), "bowType"),
-                        PersistentDataType.STRING
-                );
-
-                if (bowType == BowType.EXPLOSION.toString()) {
-                    e.getEntity().getWorld().createExplosion(e.getEntity().getLocation(), 4.0f);
-                } else if(bowType == BowType.TELEPORT.toString()) {
-                    player.teleport(e.getEntity().getLocation());
+                ItemMeta meta = bow.getItemMeta();
+                if (meta != null) {
+                    PersistentDataContainer container = meta.getPersistentDataContainer();
+                    String bowType = container.get(
+                            new NamespacedKey(TiagoUtils.getPlugin(), "bowType"),
+                            PersistentDataType.STRING
+                    );
+                    if (bowType != null) {
+                        if (bowType.equals(BowType.EXPLOSION.toString())) {
+                            e.getEntity().getWorld().createExplosion(e.getEntity().getLocation(), 3.5f);
+                        } else if (bowType.equals(BowType.TELEPORT.toString())) {
+                            player.teleport(e.getEntity().getLocation());
+                        } else if (bowType.equals(BowType.LIGHTNING.toString())) {
+                            e.getEntity().getWorld().strikeLightning(e.getEntity().getLocation());
+                        }
+                    }
                 }
             }
         }
