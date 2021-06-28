d5cc18f8fc62ae15cf0621d30578a42021db8112
Tiago-0liveira <tfgoliveira@gmail.com>
2021-06-28 11:42:54 +0100

    added final modifier

--- src/main/java/me/tiago0liveira/TiagoUtils/enums/configs/Default.java
+++ src/main/java/me/tiago0liveira/TiagoUtils/enums/configs/Default.java
@@ -1,19 +1,19 @@
 package me.tiago0liveira.TiagoUtils.enums.configs;
 
-public class Default {
-    public static String SectionCommands = "commands";
-    public static String SectionEvents = "events";
-    public static class commands {
-        public static String Fly = "Fly";
-        public static String God = "God";
-        public static String Heal = "Heal";
-        public static String ElementalBow = "ElementalBow";
-        public static String opEnchant = "opEnchant";
-        public static String setMachineGun = "setMachineGun";
+public final class Default {
+    public static final String SectionCommands = "commands";
+    public static final String SectionEvents = "events";
+    public static final class commands {
+        public static final String Fly = "Fly";
+        public static final String God = "God";
+        public static final String Heal = "Heal";
+        public static final String ElementalBow = "ElementalBow";
+        public static final String opEnchant = "opEnchant";
+        public static final String setMachineGun = "setMachineGun";
     }
-    public static class events {
-        public static String onArrowCollides = "onArrowCollides";
-        public static String onBadWeather = "onBadWeather";
-        public static String machineGuns = "machineGuns";
+    public static final class events {
+        public static final String onArrowCollides = "onArrowCollides";
+        public static final String onBadWeather = "onBadWeather";
+        public static final String machineGuns = "machineGuns";
     }
 }
