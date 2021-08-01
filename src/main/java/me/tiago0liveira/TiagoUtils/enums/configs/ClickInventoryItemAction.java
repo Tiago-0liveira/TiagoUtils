d44bf47e9470dd32d7ebb1ecabdae3c1eeb91306
Tiago-0liveira <tfgoliveira@gmail.com>
2021-08-01 19:14:37 +0100

    ClickInventoryItemAction enum to handle InventoryClicks

---
+++ src/main/java/me/tiago0liveira/TiagoUtils/enums/configs/ClickInventoryItemAction.java
@@ -0,0 +1,22 @@
+package me.tiago0liveira.TiagoUtils.enums.configs;
+
+public enum ClickInventoryItemAction {
+    MenuButton,
+    CommandsMenu,
+    EventsMenu,
+    PlayerMenu,
+    CustomEnchants;
+
+    public enum CommandsMenu {
+
+    }
+    public enum EventsMenu {
+
+    }
+    public enum PlayerMenu {
+
+    }
+    public enum CustomEnchants {
+
+    }
+}
