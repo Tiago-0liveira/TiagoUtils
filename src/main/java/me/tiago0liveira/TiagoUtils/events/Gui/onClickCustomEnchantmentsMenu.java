69f20a7a57341eb1ea669e3005b18ab4703318b9
Tiago-0liveira <tfgoliveira@gmail.com>
2021-08-29 02:19:12 +0100

    filled the base of the handler

--- src/main/java/me/tiago0liveira/TiagoUtils/events/Gui/onClickCustomEnchantmentsMenu.java
+++ src/main/java/me/tiago0liveira/TiagoUtils/events/Gui/onClickCustomEnchantmentsMenu.java
@@ -1,13 +1,33 @@
 package me.tiago0liveira.TiagoUtils.events.Gui;
 
+import me.tiago0liveira.TiagoUtils.Gui.InventoryFactory;
+import me.tiago0liveira.TiagoUtils.enums.PersistentDataManager;
+import me.tiago0liveira.TiagoUtils.helpers.RandomHelpers;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class onClickCustomEnchantmentsMenu implements Listener {
 
     @EventHandler()
     public void onClickCustomEnchantmentMenu(InventoryClickEvent event) {
+        if (event.getView().getTitle().equals(InventoryFactory.TitleMenuCustomEnchants)) {
+            event.setCancelled(true);
+            Player player = (Player) event.getWhoClicked();
+            ItemStack clickedItem = event.getCurrentItem();
 
+            if (clickedItem != null) {
+                ItemMeta clickedItemMeta = clickedItem.getItemMeta();
+                if (clickedItemMeta != null) {
+                    String unparsedClickAction = PersistentDataManager.clickAction.get(clickedItemMeta);
+                    if (!RandomHelpers.mainMenuClicked(clickedItemMeta, player)) {
+
+                    }
+                }
+            }
+        }
     }
 }
